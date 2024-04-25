b628311c25cfba4aba6e5368c345d625fa4da9c9
yahyadian <yahyadianp@gmail.com>
2024-04-25 11:31:14 +0700

    third commit

--- app/src/main/java/com/yahya/challengechapter3/ListCitiesFragment.kt
+++ app/src/main/java/com/yahya/challengechapter3/ListCitiesFragment.kt
@@ -34,7 +34,7 @@ class ListCitiesFragment : Fragment() {
         fragmentBinding.rvListCities.setHasFixedSize(true)
 
         val arguments = arguments?.getString(KEY)
-        var wordsLists = arrayListOf<String>()
+        var alphabetsList = arrayListOf<String>()
         val title = "List of cities from $arguments"
         (requireActivity() as AppCompatActivity).supportActionBar?.apply {
             setTitle(title)
@@ -43,107 +43,107 @@ class ListCitiesFragment : Fragment() {
 
         when(arguments) {
             Cities.A.char -> {
-                wordsLists = Cities.A.listCities
+                alphabetsList = Cities.A.listCities
             }
 
             Cities.B.char -> {
-                wordsLists = Cities.B.listCities
+                alphabetsList = Cities.B.listCities
             }
 
             Cities.C.char -> {
-                wordsLists = Cities.C.listCities
+                alphabetsList = Cities.C.listCities
             }
 
             Cities.D.char -> {
-                wordsLists = Cities.D.listCities
+                alphabetsList = Cities.D.listCities
             }
 
             Cities.E.char -> {
-                wordsLists = Cities.E.listCities
+                alphabetsList = Cities.E.listCities
             }
 
             Cities.F.char -> {
-                wordsLists = Cities.F.listCities
+                alphabetsList = Cities.F.listCities
             }
 
             Cities.G.char -> {
-                wordsLists = Cities.G.listCities
+                alphabetsList = Cities.G.listCities
             }
 
             Cities.H.char -> {
-                wordsLists = Cities.H.listCities
+                alphabetsList = Cities.H.listCities
             }
 
             Cities.I.char -> {
-                wordsLists = Cities.I.listCities
+                alphabetsList = Cities.I.listCities
             }
 
             Cities.J.char -> {
-                wordsLists = Cities.J.listCities
+                alphabetsList = Cities.J.listCities
             }
 
             Cities.K.char -> {
-                wordsLists = Cities.K.listCities
+                alphabetsList = Cities.K.listCities
             }
 
             Cities.L.char -> {
-                wordsLists = Cities.L.listCities
+                alphabetsList = Cities.L.listCities
             }
 
             Cities.M.char -> {
-                wordsLists = Cities.M.listCities
+                alphabetsList = Cities.M.listCities
             }
 
             Cities.N.char -> {
-                wordsLists = Cities.N.listCities
+                alphabetsList = Cities.N.listCities
             }
 
             Cities.O.char -> {
-                wordsLists = Cities.O.listCities
+                alphabetsList = Cities.O.listCities
             }
 
             Cities.P.char -> {
-                wordsLists = Cities.P.listCities
+                alphabetsList = Cities.P.listCities
             }
 
             Cities.Q.char -> {
-                wordsLists = Cities.Q.listCities
+                alphabetsList = Cities.Q.listCities
             }
 
             Cities.R.char -> {
-                wordsLists = Cities.R.listCities
+                alphabetsList = Cities.R.listCities
             }
 
             Cities.S.char -> {
-                wordsLists = Cities.S.listCities
+                alphabetsList = Cities.S.listCities
             }
 
             Cities.T.char -> {
-                wordsLists = Cities.T.listCities
+                alphabetsList = Cities.T.listCities
             }
 
             Cities.U.char -> {
-                wordsLists = Cities.U.listCities
+                alphabetsList = Cities.U.listCities
             }
 
             Cities.V.char -> {
-                wordsLists = Cities.V.listCities
+                alphabetsList = Cities.V.listCities
             }
 
             Cities.W.char -> {
-                wordsLists = Cities.W.listCities
+                alphabetsList = Cities.W.listCities
             }
 
             Cities.X.char -> {
-                wordsLists = Cities.X.listCities
+                alphabetsList = Cities.X.listCities
             }
 
             Cities.Y.char -> {
-                wordsLists = Cities.Y.listCities
+                alphabetsList = Cities.Y.listCities
             }
 
             Cities.Z.char -> {
-                wordsLists = Cities.Z.listCities
+                alphabetsList = Cities.Z.listCities
             }
         }
 
@@ -152,7 +152,7 @@ class ListCitiesFragment : Fragment() {
             intent.setData(Uri.parse("https://google.com/search?q=$it"))
             activity?.startActivity(intent)
         }
-        adapter.submitList(wordsLists)
+        adapter.submitList(alphabetsList)
         fragmentBinding.rvListCities.also {
             it.adapter = adapter
             it.layoutManager = LinearLayoutManager(activity)
